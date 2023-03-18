18893fc40e1b04ca41105504fe777a959918e4cc
Artur Rustamhanov <a.rustamkhanov@astondevs.ru>
2023-03-18 19:00:45 +0300

    Gotovo1

---
+++ src/main/java/org/example/HomeWorkApp.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        printColor();
+        checkSumSign();
+        compareNumbers();
+    }
+
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign(){
+        int a = 5;
+        int b = 3;
+        if (a+b>=0){
+            System.out.println("Сумма положительная");
+        }
+        else System.out.println("Сумма отрицательная");
+    }
+
+    public static void printColor(){
+        int value = 85;
+        if (value<=0){
+            System.out.println("Красный");
+        } else if (value<100 && value>0){
+            System.out.println("Желтый");
+        } else System.out.println("Красный");
+    }
+
+    public static void compareNumbers(){
+        int a = 5;
+        int b = 3;
+        if (a>=b){
+            System.out.println("a >= b");
+        } else
+            System.out.println("a < b");
+    }
+}
