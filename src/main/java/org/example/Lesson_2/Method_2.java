79c0039d03b4ecdca1db0c3aec9f54126b3fd9f4
Artur Rustamhanov <a.rustamkhanov@astondevs.ru>
2023-04-08 22:24:48 +0300

    change Method2

--- src/main/java/org/example/Lesson_2/Method_2.java
+++ src/main/java/org/example/Lesson_2/Method_2.java
@@ -1,13 +1,6 @@
 package org.example.Lesson_2;
 
 public class Method_2 {
-
-//    public static void main(String[] args) {
-////        System.out.println(Arrays.toString(MethodSeven.checkAndMultiplyByTwo(ArrayThree,6)));
-////        Method8.fillingSquareArray(3);
-////        Method9.createArray(5, 8);
-////        Method_10.shiftArray(ArrayOne,-2);
-//    }
     public static void checkOfPositive(int a){
         if (a>=0){
             System.out.println("Положительное число");
